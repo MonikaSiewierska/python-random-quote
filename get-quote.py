daddbf86c774db297766895d5221efa007867384
MonikaSiewierska <Monika.siewierska1@gmail.com>
2020-11-16 13:29:41 +0100

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -2,13 +2,20 @@ import random
 def primary():
   # print("Keep it logically awesome.")
 
+  f = open("quotes.txt", "a")
+  f.write("PIERWSZY DDD \n")
+  f.write("DRUGI DDD \n")
+  f.write("TRZECI DDD \n")
+  f.close()
+  
   f = open("quotes.txt")
   quotes = f.readlines()
-  f.close()
+  f.close()  
 
   last = len(quotes)-1
   rnd = random.randint(0,last)
-  print(quotes[rnd])
+  rnd2 = random.randint(0,last)
+  print(quotes[rnd], quotes[rnd2])
 
 if __name__== "__main__":
   primary()
